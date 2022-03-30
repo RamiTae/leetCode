4ce4136a6811ead3e7ce4d9b5ddd1ea494796680
태라미 <53169445+RamiTae@users.noreply.github.com>
2022-03-31 00:15:24 +0900

    Time: 64 ms (98.88%), Space: 45.2 MB (34.08%) - LeetHub

---
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.ts
@@ -0,0 +1,33 @@
+// Definition for singly-linked list.
+// class ListNode {
+//   val: number;
+//   next: ListNode | null;
+//   constructor(val?: number, next?: ListNode | null) {
+//     this.val = val === undefined ? 0 : val;
+//     this.next = next === undefined ? null : next;
+//   }
+// }
+
+function mergeTwoLists(
+  list1: ListNode | null,
+  list2: ListNode | null
+): ListNode | null {
+  if (!list1 && !list2) {
+    return null;
+  } else if (!list1 && list2) {
+    return list2;
+  } else if (!list2 && list1) {
+    return list1;
+  }
+
+  const nodeVal1: number = list1.val;
+  const nodeVal2: number = list2.val;
+
+  let result: ListNode;
+  if (nodeVal1 < nodeVal2) {
+    result = new ListNode(nodeVal1, mergeTwoLists(list1.next, list2));
+  } else {
+    result = new ListNode(nodeVal2, mergeTwoLists(list1, list2.next));
+  }
+  return result;
+}
