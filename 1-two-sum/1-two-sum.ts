c9c3eeb21c875a21b13cbd2f40b3126fafccb64e
태라미 <53169445+RamiTae@users.noreply.github.com>
2022-04-09 16:54:58 +0900

    Time: 100 ms (72.36%), Space: 45.4 MB (37.40%) - LeetHub

--- 1-two-sum/1-two-sum.ts
+++ 1-two-sum/1-two-sum.ts
@@ -1,7 +1,6 @@
 function twoSum(nums: number[], target: number, startIdx: number = 0): number[] {
-  let prevIdx: number = 0;
   for(let idx:number = 1; idx < nums.length; idx++) {
-    if(nums[prevIdx] + nums[idx] === target) {
+    if(nums[0] + nums[idx] === target) {
       return [startIdx, startIdx + idx];
     }
   }
